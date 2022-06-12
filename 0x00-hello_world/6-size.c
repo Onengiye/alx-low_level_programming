77540660bdac0e0df5cb5a8e7a279796f8cc870a
Onengiye <amadionengiyewilliams@gmail.com>
2022-06-12 16:33:27 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point, to print sizeof data types
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	int i;
+	char c;
+	long-int li;
+	long-long ll;
+	float f;
+	printf("Size of a char: %c", sizeof(c));
+	printf("Size of an int: %i", sizeof(i));
+	printf("Size of a long int: %i", sizeof(li));
+	printf("Size of a long long int: %i", sizeof(ll));
+	printf("Size of a float: %f", sizeof(f));
+	return (0);
+}
