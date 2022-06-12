32390afd37a324c1042cbecd84951bfa51dce65e
Onengiye <amadionengiyewilliams@gmail.com>
2022-06-12 16:46:35 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,17 +4,17 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
+{
 	int i;
 	char c;
 	long int li;
 	long long int ll;
 	float f;
 
-	printf("Size of a char: %lu", sizeof(c));
-	printf("Size of an int: %lu", sizeof(i));
-	printf("Size of a long int: %lu", sizeof(li));
-	printf("Size of a long long int: %lu", sizeof(ll));
-	printf("Size of a float: %lu", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
