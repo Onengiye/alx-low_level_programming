514fdee1e708e42c2a2220e8c34c7f0c2b646d68
Onengiye <amadionengiyewilliams@gmail.com>
2022-06-13 15:50:55 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print all the alphabet from a..z except q and e
+ *
+ * Description: All in lowercase, print all the alphabet fro a..z
+ *
+ * Return: Success 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n <= 9)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
