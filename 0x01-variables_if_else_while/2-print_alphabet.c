a30bc558630518b68f67f42344ffc7f600e31455
Onengiye <amadionengiyewilliams@gmail.com>
2022-06-13 15:16:44 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print all the alphabet from a..z
+ *
+ * Description: All in lowercase, print all the alphabet fro a..z
+ *
+ * Return: Success 0
+ */
+int main(void)
+{
+	char n;
+	
+	n = 'a';
+	while(c <= 'z')
+	{
+		putchar('n');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
