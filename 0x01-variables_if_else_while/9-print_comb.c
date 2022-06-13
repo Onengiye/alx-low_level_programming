eeff8b234c1a17912d9a718016d96b711a3f6801
Onengiye <amadionengiyewilliams@gmail.com>
2022-06-13 16:22:04 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print all the alphabet from a..z except q and e
+ *
+ * Description: All in lowercase, print all the alphabet fro a..z
+ *
+ * Return: Success 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n <= 9)
+	{
+		putchar(n + '0');
+		putchar(',');
+		putchar(' ');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
