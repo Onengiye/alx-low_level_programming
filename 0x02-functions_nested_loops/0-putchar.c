8536ba382e0876da17a49d8c915dd9daa384d560
Onengiye <amadionengiyewilliams@gmail.com>
2022-06-15 13:48:12 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include "main.h"
 /**
  * main - writes the character c to stdout
  *
@@ -10,6 +11,16 @@
  */      
 int main(void)
 {
-	_putchar("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
