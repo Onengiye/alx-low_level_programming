d69b76eb4ee73e966053de34221f08250558bcc5
Onengiye <amadionengiyewilliams@gmail.com>
2022-06-15 16:32:15 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,15 +3,16 @@
 
 /*
  * _islower - check if a character is lowercase
- *
  * @c: args
  *
+ * Description: Using the ctype header file
  * Return: Return 1 if the character is lower case
  * and 0 if the character is uppercase
  */
 int _islower(int c)
 {
 	int a = islower(c);
+
 	if (a > 0)
 	{
 		return (1);
