1872ca714205d5ac12a75d016ad57c84552c7995
Onengiye <amadionengiyewilliams@gmail.com>
2022-06-15 16:22:10 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <stdio>
+
+/*
+ * _islower - check if a character is lowercase
+ *
+ * @c: args
+ *
+ * Return: Return 1 if the character is lower case
+ * and 0 if the character is uppercase
+ */
+int _islower(int c)
+{
+	int a = islower(c);
+	if (a > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
