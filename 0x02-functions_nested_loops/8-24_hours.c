fc8837b8bd5f48e845e6f51206b2a43ec099585e
Onengiye <amadionengiyewilliams@gmail.com>
2022-06-16 04:56:50 -0700

    0-main.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * jack_bauer - Prints the last digit of a number
+ * @n: args
+ *
+ * Return: Return 1 if the number is greater than 0
+ * and 0 if the number is zero
+ */
+void jack_bauer(void)
+{
+	int hr = 000;
+	int min = 000;
+
+	while (h < 24)
+	{
+		while (min <= 60)
+		{
+
+
+
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+# Debuggging 
+process of finding and fixing errors in software that preventa it from running correctly
+ALX school, <80%
