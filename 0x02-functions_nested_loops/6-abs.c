12dd92e49b32b5796123f7275ff0015bae467347
Onengiye <amadionengiyewilliams@gmail.com>
2022-06-16 02:32:43 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - print the sign of a number
+ * @n: args
+ *
+ * Return: Return 1 if the number is greater than 0
+ * and 0 if the number is zero
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
