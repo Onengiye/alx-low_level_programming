4089f9c15aa2fbc8f107b3997a3ba2d409b6b027
Onengiye <amadionengiyewilliams@gmail.com>
2022-06-16 04:07:17 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
 /**
  * add - Adds two integers and return the result
  * @c: first integer
- * @c: second integer
+ * @d: second integer
  *
  * Description: Adds two integers and return the result
  * Return: Return the result of the addition
