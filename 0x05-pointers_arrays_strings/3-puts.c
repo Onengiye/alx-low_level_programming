34b15e2b0dfc2e67c843871c53c19b173f1198ce
Onengiye <amadionengiyewilliams@gmail.com>
2022-06-16 19:28:24 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: pointer parameters
+ *
+ * Return: Length of string
+*/
+void _puts(char *str)
+{
+	puts(str);
+}
