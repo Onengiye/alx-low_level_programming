5f14d5a2b12081f6951416f8e9f90b7935b51e6e
Onengiye <amadionengiyewilliams@gmail.com>
2022-06-16 20:28:45 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - Compares two strings 
+ * @s1: Parameter
+ * @s2: Parameter
+ *
+ * Return:int
+*/
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
